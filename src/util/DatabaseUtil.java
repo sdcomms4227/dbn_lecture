50df002212ab9e21c2b7838bfea91f912cafe201
itwill4227 <itwill4227@gmail.com>
2020-05-04 10:29:06 +0900

    change db connection

--- src/util/DatabaseUtil.java
+++ src/util/DatabaseUtil.java
@@ -7,10 +7,10 @@ public class DatabaseUtil {
 	public static Connection getConnection() {
 		try {
 			String dbURL = "jdbc:mysql://localhost:3307/lecture";
-			String dbID = "root";
-			String dbPassword = "tkddn12!@";
+			String dbID = "dbn";
+			String dbPassword = "1234";
 			Class.forName("com.mysql.jdbc.Driver");
-			return DriverManager.getConnection(dbURL,dbID,dbPassword);
+			return DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
